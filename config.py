2b057f9e2c6e2ed98286c071306409172049356b
ckzone-pc <1171284619@qq.com>
2018-05-17 13:50:45 +0800

    创建配置

--- config.py
+++ config.py
@@ -1,3 +1,20 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = r'\xa7\xdb\xfd\xcetp\xf2\x9df+\xe20\x92\x1f\x18\x90\xcb\x0b\xd8RY$3\x8b'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    
+    # 配置邮件信息
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+
+
