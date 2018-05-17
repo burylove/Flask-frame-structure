4c03d8b47fcdb83d45c68292f7730988a5d53882
ckzone-pc <1171284619@qq.com>
2018-05-17 14:13:59 +0800

    设置默认值

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    #r'\xa7\xdb\xfd\xcetp\xf2\x9df+\xe20\x92\x1f\x18\x90\xcb\x0b\xd8RY$3\x8b'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or \
+    r'\xa7\xdb\xfd\xcetp\xf2\x9df+\xe20\x92\x1f\x18\x90\xcb\x0b\xd8RY$3\x8b'
+    
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     
     # 配置邮件信息
@@ -24,23 +25,22 @@ class DevelopmentConfig(Config):
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_USERNAME = "zzz1171284619@gmail.com"
-    MAIL_PASSWORD = "zcf.19970125"
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or "zzz1171284619@gmail.com"
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or "zcf.19970125"
 
     #数据库
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') 
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:12345679@localhost:3306/my_blog'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql://root:12345679@localhost:3306/my_blog'
 
 
 class TestingConfig(Config):
     TESTING = True
-   # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') 
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:12345679@localhost:3306/my_blog'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql://root:12345679@localhost:3306/my_blog'
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql://root:12345679@localhost:3306/my_blog'
 
 config = {
     'development': DevelopmentConfig,
