c76202b32eaba58e51451511a2effad5c6df75a0
ckzone-pc <1171284619@qq.com>
2018-05-17 16:12:23 +0800

    commit

--- flask/app/main/views.py
+++ flask/app/main/views.py
@@ -10,27 +10,6 @@ from .. import db
 from ..models import User
 
 
-# 自定义对象
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(64), unique = True)
-    users = db.relationship('User', backref = 'role',lazy = 'dynamic')
-    #  backref 参数向 User 模型中添加一个 role 属性，从而定义反向关系
-
-    def __repr__(self):
-        return '<Role %r >' % self.name
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(64), unique = True, index = True )
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))  # 设置外键
-
-    def __repr__(self):
-        return '<User %r >' % self.username
-
 
 # 定义表单
 class NameForm(Form):
